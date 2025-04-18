33be1ec1ff28063760673cf96e05fc3f4e5b0efd
InJamong <shindh0112@gmail.com>
2025-04-18 09:23:39 +0900

    TEXTRPG
    뭘 수정했는지 적는곳.

---
+++ TEXT RPG 만들기/TEXT RPG 만들기/MainGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXT_RPG_만들기
+{
+    internal class MainGame
+    {
+    }
+}
